c8274e00af7819172eb07885559882adef49a8d0
Magnus Haggren <barcelonaguiden@gmail.com>
2023-02-01 16:48:32 +0100

    changed server config

--- config/index.js
+++ config/index.js
@@ -1,5 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev
-  ? 'http://localhost:3000'
-  : 'https://www.barcelona-reseguide.se'
+export const server = dev ? 'http://localhost:3000' : 'https://www.vercel.com'
