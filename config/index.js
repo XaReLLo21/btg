708120323d9a1de6da31af7c93501c8e0624be19
Magnus Haggren <barcelonaguiden@gmail.com>
2023-02-03 12:26:36 +0100

    ver 1.2

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production'
 
 export const server = dev
   ? 'http://localhost:3000'
-  : 'https://vercel.com/xarello21/btg'
+  : 'https://reseguide.vercel.app'
